ca2e251524531d4479ad2f3732523b22c32b5399
Andrew <kovacz@ukr.net>
2018-03-05 16:36:49 +0200

    upd

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ private:
 		color check_color(uns r, uns g, uns b)
 		{
 			uns breaking_values[][4] = {
-				  0,   0,	0, color::black,
+				  0,   0,   0, color::black,
 				  0,   0, 255, color::blue,
 				  0, 255,   0, color::green,
 				255,   0,   0, color::red,
@@ -74,11 +74,11 @@ private:
 	static const uns N = 5;
 	Pixel pixels[N] 
 	{
-		Pixel(0,	 0,	  0),
-		Pixel(255,	 0,   0),
+		Pixel(  0,  0,    0),
+		Pixel(255,   0,   0),
 		Pixel(255, 255,	255),
-		Pixel(0,   255,	  0),
-		Pixel(255,	 0,	  0)
+		Pixel(  0, 255,   0),
+		Pixel(255,   0,   0)
 	};
 };
 
